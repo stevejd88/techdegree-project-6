fb462b05fa9eb53ad58499685de0790f3245be50
Steven De Avila <stevejd88@gmail.com>
2019-07-11 13:53:01 -0500

    created app.js & added evenhandler to hide overlay

---
+++ app.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const qwerty = document.getElementById('qwerty');
+  const phrase = document.getElementById('phrase');
+  let missed = 0;
+
+  const overlay = document.getElementById('overlay');
+  const btnReset = document.querySelector('.btn__reset');
+
+  overlay.addEventListener('click', (e) => {
+    overlay.style.display = 'none';
+
+  });
+
+
+
+
+
+
+
+});
