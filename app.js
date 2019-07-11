f02268c4a32f47d541e6fb31f6c2f1026b946bb8
Steven De Avila <stevejd88@gmail.com>
2019-07-11 14:10:37 -0500

    created array of phrases

--- app.js
+++ app.js
@@ -6,9 +6,17 @@ document.addEventListener('DOMContentLoaded', () => {
   const overlay = document.getElementById('overlay');
   const btnReset = document.querySelector('.btn__reset');
 
+  const phrases = [
+    `All that and a bag of chips`,
+    `Talk to the Hand`,
+    `Take A Chill Pill`,
+    `Eat My Shorts`,
+    `Hasta La Vista Baby`
+  ];
+
+
   overlay.addEventListener('click', (e) => {
     overlay.style.display = 'none';
-
   });
 
 
